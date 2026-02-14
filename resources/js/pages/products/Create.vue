9b99a1d9a4a652ae5b41d0259c663553376dd2b7
Pat-HiPE <patrick_louis_rivera@hipe.asia>
2026-02-14 23:18:09 +0800

    initial commit

---
+++ resources/js/pages/products/Create.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Create Products',
+        href: '/products/create',
+    },
+];
+</script>
+
+<template>
+    <Head title="Create Product" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div>
+            Create product form coming soon...
+        </div>
+    </AppLayout>
+</template>
---
+++ resources/js/pages/products/Index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Products',
+        href: '/products',
+    },
+];
+</script>
+
+<template>
+    <Head title="Products" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div>
+            Products list coming soon...
+        </div>
+    </AppLayout>
+</template>
